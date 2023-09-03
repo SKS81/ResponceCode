7c509e8f916c2808f84b42212c7d1141f7681c9b
Aleksandr Vedeneev <114874014+SKS81@users.noreply.github.com>
2023-09-03 16:58:04 +0300

    Add files via upload

---
+++ src/responce.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class responce {
+
+    public static void main(String[] args) {
+        window w = new window("Коды ответа @BY SKS");
+        w.setVisible (true);
+        w.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+        w.setSize(750, 150);
+    }
+}
